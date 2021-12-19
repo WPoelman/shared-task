43c7489937a9ba4baa4ca5a953323e4788ae52d5
esther2000 <49815271+esther2000@users.noreply.github.com>
2021-12-19 13:05:21 +0100

    Add functionality for making FR and IT splits

--- tools/create_en_data_split.py
+++ tools/create_en_data_split.py
@@ -13,18 +13,31 @@ from nltk import pos_tag
 
 def main():
 
-    # Open file as list to allow for multiple iteration
-    f = open("subtask_1_en.csv").read().splitlines()
+    # Open files as list to allow for multiple iteration
+    with open("subtask_1_en.csv") as f1, open("subtask_1_fr.csv") as f2, open("subtask_1_it.csv") as f3:
+        f_en = f1.read().splitlines()  # English
+        f_fr = f2.read().splitlines()  # French
+        f_it = f3.read().splitlines()  # Italian
 
     # Get person and material nouns
-    spec_nouns = get_special_nouns(f)
+    spec_nouns = get_special_nouns(f_en)
 
-    # Generate splits
-    test_lines = create_test_set(f, spec_nouns)
-    write_to_file(test_lines, "en_new_test_split.csv")
+    # Generate splits for English
+    en_test_lines = create_test_set(f_en, spec_nouns)
+    write_to_file(en_test_lines, "en_new_test_split.csv")
+    write_to_file(create_train_set(f_en, en_test_lines), "en_new_train_split.csv")
 
-    train_lines = create_train_set(f, test_lines)
-    write_to_file(train_lines, "en_new_train_split.csv")
+    test_ids = [line.split(",")[0] for line in en_test_lines]
+
+    # Generate splits for French
+    fr_test_lines, fr_train_lines = make_splits_from_ids(f_fr, test_ids)
+    write_to_file(fr_test_lines, "fr_new_test_split.csv")
+    write_to_file(fr_train_lines, "fr_new_train_split.csv")
+
+    # Generate splits for Italian
+    it_test_lines, it_train_lines = make_splits_from_ids(f_it, test_ids)
+    write_to_file(it_test_lines, "it_new_test_split.csv")
+    write_to_file(it_train_lines, "it_new_train_split.csv")
 
 
 def create_test_set(file, nouns):
@@ -66,6 +79,15 @@ def write_to_file(lines, outfile):
         for line in set(lines):
             if "id,sentence,labels" not in line:
                 o.write(line + "\n")
+
+
+def make_splits_from_ids(f, ids):
+    """ Generate splits for given the ids of the lines that should
+    only occur in the test set (to be used for French and Italian here) """
+    test_lines = [line for line in f if line.split(",")[0] in ids]
+    train_lines = create_train_set(f, test_lines)
+
+    return test_lines, train_lines
 
 
 if __name__ == "__main__":
