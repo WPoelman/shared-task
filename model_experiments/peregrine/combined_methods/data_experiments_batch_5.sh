ad2b17cd46972ce33ed30de6430037f26df6fb9e
Wessel Poelman <contact+github@wesselpoelman.nl>
2022-01-19 14:03:29 +0100

    dumb way to submit separate jobs

--- model_experiments/peregrine/combined_methods/data_experiments_batch_5.sh
+++ model_experiments/peregrine/combined_methods/data_experiments_batch_5.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=data_experiment_job
+#SBATCH --job-name=dataset_5
 #SBATCH --time=06:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100:1
@@ -8,6 +8,6 @@
 module load PyTorch/1.6.0-fosscuda-2019b-Python-3.7.4
 pip install --user -r requirements.txt
 python data_experiments.py \
-    --data_directory /home/$USER/shared-task/data_experiments/combined_methods  \
+    --data_directory /home/$USER/shared-task/data_experiments/combined_methods/dataset_5 \
     --test_set /home/$USER/shared-task/data_experiments/en_new_test_split.csv \
     --output_dir /data/$USER/shared-task-output/combined_methods
